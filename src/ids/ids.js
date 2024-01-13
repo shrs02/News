6373c7f895116fd2c95667908423da316eb5c6a1
Your Name <shrds839@gmail.com>
2024-01-13 17:12:47 +0530

    first commit

---
+++ src/ids/ids.js
@@ -0,0 +1,8 @@
+const ids = {
+    Url: String(import.meta.env.VITE_URL),
+    PId: String(import.meta.env.VITE_PID),
+    DId: String(import.meta.env.VITE_DID),
+    CId: String(import.meta.env.VITE_CID),
+}
+
+export default ids
